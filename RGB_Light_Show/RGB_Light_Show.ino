98c124231b5fd2fc2c8c8c0f1a0b04f306988272
Bjorn Sodergren <bjorn.sodergren@gmail.com>
2012-10-09 02:56:53 -0500

    added loop

--- RGB_Light_Show/RGB_Light_Show.ino
+++ RGB_Light_Show/RGB_Light_Show.ino
@@ -16,14 +16,11 @@ Shifter shifter(SER_Pin, RCLK_Pin, SRCLK_Pin, NUM_REGISTERS);
 int latchPin = 8;
 int clockPin = 12;
 int dataPin = 13;
-int i = 0;
 
 int buttonInt = 0; //AKA digital pin 2
 volatile int selectedMode = 1;
 
-
 int sensorPin = A0;    // select the input pin for the potentiometer
-
 int sensorValue = 0; 
 int delayValue = 0;
 
@@ -43,65 +40,119 @@ void setup() {
 }
 void swap()
 {
+  if(selectedMode < 5)
+  {
+    selectedMode++;
+  }
+  else {
+    selectedMode=1;
+  }
+  Serial.println(selectedMode);
+
 }
 
 int delaySpeed()
 {
-   sensorValue = analogRead(sensorPin); 
+  sensorValue = analogRead(sensorPin); 
   delayValue = map(sensorValue, 0, 1023, 20, 500); 
+  //Serial.println(delayValue);
+
   return delayValue;
 }
 
 void rotate()
 {
-  for (i = 0; i < 8; i++) {
-    shifter.clear(); //set all pins on the shift register chain to LOW
-    shifter.setPin(i,HIGH); //set pin 1 in the chain(second pin) HIGH
 
-      if( i % 2) {
-      red();
-    } 
-    else {
-      blue();
-    }
-    shifter.write(); //send changes to the chain and display the 
+  red();
+  delay(delaySpeed());
+  blue(); 
+  delay(delaySpeed());
+  green();
+  delay(delaySpeed());
+}
 
-      delay(delaySpeed());
+
+void loop()
+{
+
+  switch (selectedMode)
+  {
+  case 1:
+    red();
+    break;
+
+  case 2:
+    blue();
+    break;
+
+  case 3:
+    green();
+    break;
+
+  case 4:
+    white();
+    break;
+
+  case 5:
+    rotate();
+    break;
+
+  case 6:
+    off();
+    break;
 
   }
 }
 
 
-void loop() {
-
- 
-  
-  
-  
-  
-
-}
-
-
 void white()
 {
+  shifter.clear();
   analogWrite(REDPIN, 255);
   analogWrite(GREENPIN, 255);
   analogWrite(BLUEPIN, 255);
-
+  shifter.setAll(HIGH);
+  shifter.write();
 }  
 void red()
 {
+  shifter.clear();
   analogWrite(REDPIN, 225);
   analogWrite(GREENPIN, 0);
   analogWrite(BLUEPIN, 0);
+  shifter.setAll(HIGH);
+  shifter.write();
 }
 
 void blue()
 {
+  shifter.clear();
   analogWrite(REDPIN, 0);
   analogWrite(GREENPIN, 0);
   analogWrite(BLUEPIN, 255);
+  shifter.setAll(HIGH);
+  shifter.write();
 }
+
+void green()
+{
+  shifter.clear();
+  analogWrite(REDPIN, 0);
+  analogWrite(GREENPIN, 255);
+  analogWrite(BLUEPIN, 0);
+  shifter.setAll(HIGH);
+  shifter.write();
+}
+
+void off()
+{
+  shifter.clear();
+
+  shifter.setAll(LOW);
+  shifter.write();
+}
+
+
+
 
 
